34de0c12ec3dc7742119d9013ec7d5f7e23016b8
RealityStop <realitydotstop@gmail.com>
2025-05-12 00:15:08 -0400

    A little header, which gets shown in the add node dialog

--- addons/NinePathContainer/nine_patch_container.gd
+++ addons/NinePathContainer/nine_patch_container.gd
@@ -1,3 +1,6 @@
+## A Container that places content inside of a customizable nine-patch,
+## with support for custom background, frame, and content margins.
+
 ## This Source Code Form is subject to the terms of the Mozilla Public
 ## License, v. 2.0. If a copy of the MPL was not distributed with this
 ## file, You can obtain one at https://mozilla.org/MPL/2.0/.
