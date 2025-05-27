8dc310f61685aec029e81bc63e72d6fe36377d73
RealityStop <realitydotstop@gmail.com>
2025-05-26 20:57:21 -0400

    added icon and fixed folder name

--- addons/NinePatchContainer/nine_patch_container_plugin.gd
+++ addons/NinePatchContainer/nine_patch_container_plugin.gd
@@ -5,7 +5,7 @@ extends EditorPlugin
 func _enter_tree():
 	# Initialization of the plugin goes here.
 	# Add the new type with a name, a parent type, a script and an icon.
-	add_custom_type("NinePatchContainer", "MarginContainer", preload("res://addons/NinePathContainer/nine_patch_container.gd"), null)
+	add_custom_type("NinePatchContainer", "MarginContainer", preload("res://addons/NinePatchContainer/nine_patch_container.gd"), preload("res://addons/NinePatchContainer/nine_patch_container_icon.svg"))
 
 
 func _exit_tree():
